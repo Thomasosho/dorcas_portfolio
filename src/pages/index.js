ccd37af0226dbd66c4d2442aaf58a977cf72c62c
Thomasosho <tomasosho@gmail.com>
2024-01-19 15:26:33 +0100

    metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,18 @@ import Portfolio from "@/components/portfolio";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
+export const metadata = {
+  title: 'Dorcas Charles',
+  description: '﹒UI/UX DESIGNER﹒GRAPHICS DESIGNER﹒LOGO DESIGNER﹒',
+  metadataBase: new URL('https://dorcas-portfolio.vercel.app'),
+  image: '/dorcasOne.png',
+};
+
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen w-screen flex-col items-center justify-between`}
     >
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <Head />
       <Offer />
