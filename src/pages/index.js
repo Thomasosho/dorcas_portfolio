3878d14220fba0e0704bbc5e40e93a2cf2bb57d0
Thomasosho <tomasosho@gmail.com>
2024-01-21 21:59:24 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export const metadata = {
   title: "Dorcas Charles",
   description: "﹒UI/UX DESIGNER﹒GRAPHICS DESIGNER﹒LOGO DESIGNER﹒",
   metadataBase: new URL("https://dorcas-portfolio.vercel.app"),
-  image: "/dorcasOne.png",
+  image: "https://mir-s3-cdn-cf.behance.net/user/230/1de0f91047973097.62c693ebd8abb.jpg",
 };
 
 export default function Home() {
